c20de9f9e2402d97e296157a286637a8e1f16934
frost-rvl <137229836+frost-rvl@users.noreply.github.com>
2026-01-27 20:11:37 +0100

    Setup python packages with flake.nix

---
+++ README.md
@@ -0,0 +1,2 @@
+# Digital_Creativity_Final_Assignement
+Working on 
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flask gallery app for digital creativity projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python312
+            pkgs.python312Packages.flask
+          ];
+        };
+      }
+    );
+}
