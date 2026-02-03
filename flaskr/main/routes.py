62cbeccb7435d806ef4c9c9acf863b697860c0b3
frost-rvl <137229836+frost-rvl@users.noreply.github.com>
2026-02-03 18:07:35 +0100

    feat: Add profile page

--- flaskr/main/routes.py
+++ flaskr/main/routes.py
@@ -1,7 +1,10 @@
+import sqlalchemy as sa
 from flask import render_template
-from flask_login import current_user
+from flask_login import current_user, login_required
 
+from flaskr import db
 from flaskr.main import bp
+from flaskr.models import User
 
 
 @bp.route("/")
@@ -10,3 +13,10 @@ def index():
     if not current_user.is_authenticated:
         return render_template("landing.html")
     return render_template("index.html", title="Home")
+
+
+@bp.route("/user/<username>")
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    return render_template("user.html", user=user)
