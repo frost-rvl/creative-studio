437ef60fe3b9fc388fc8b1286e85e5cf50cf3334
frost-rvl <137229836+frost-rvl@users.noreply.github.com>
2026-02-05 15:46:14 +0100

    feat: Add error features
    -add 404 and 500 html pages
    -add email notifications for ADMINS
    -add file logs

--- flaskr/profile/forms.py
+++ flaskr/profile/forms.py
@@ -1,8 +1,12 @@
+import sqlalchemy as sa
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed
+from flask_wtf.file import FileAllowed, ValidationError
 from wtforms import FileField, StringField, TextAreaField
 from wtforms.fields.simple import SubmitField
 from wtforms.validators import DataRequired, Length
+
+from flaskr import db
+from flaskr.models import User
 
 
 class EditProfileForm(FlaskForm):
@@ -21,3 +25,15 @@ class EditProfileForm(FlaskForm):
     )
     about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
     submit = SubmitField("Save changes")
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    # def validate_username(self, username):
+    #     if username.data != self.original_username:
+    #         user = db.session.scalar(
+    #             sa.select(User).where(User.username == username.data)
+    #         )
+    #         if user is not None:
+    #             raise ValidationError("Please use a different username.")
